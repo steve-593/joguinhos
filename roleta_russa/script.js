b6db647713f0175a4755e48d8ad29f93e8a358b4
steve-593 <igorsantanan@hotmail.com>
2024-10-20 18:35:08 -0300

    adicionei audios ao jogo

--- roleta_russa/script.js
+++ roleta_russa/script.js
@@ -8,8 +8,11 @@ const numeroAleatorio = getRandomInt(0, 5);
 
 const mostraNum = document.getElementById('mostraNum');
 const btn = document.getElementById('btn');
+const girar = document.getElementById('girar')
+const tambor = document.getElementById('tambor')
 
 let tentativaAtual = 0;
+let anguloAtual = 0
 
 function verificar() {
     
@@ -20,11 +23,23 @@ function verificar() {
         alert('Voce morreu!');
         btn.disabled = true;
     } else {
+        document.getElementById('empty').play();
         tentativaAtual += 1;
         mostraNum.innerText = `${tentativaAtual}/6`
+        anguloAtual += 60;
+        tambor.style.transform = `translate(-50%, -50%) rotate(${anguloAtual}deg)`;
+        tambor.style.transition = '1s'
     }
 }
 
+function girarTambor () {
+    anguloAtual += 60;
+    document.getElementById('somReload').play();
+    tambor.style.transform = `translate(-50%, -50%) rotate(${anguloAtual}deg)`;
+    tambor.style.transition = '1s'
+}
+
 btn.addEventListener('click', verificar);
+girar.addEventListener('click', girarTambor)
 
 console.log(numeroAleatorio)
